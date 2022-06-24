d770dc42c79ca4a444e4434094f3ee2ba29d31ff
dirkdog <leveluforums@gmail.com>
2022-06-24 16:25:24 -0600

    Add Penguin games Berg

--- src/scenes/rooms/old/berg/pg/Berg.js
+++ src/scenes/rooms/old/berg/pg/Berg.js
@@ -12,6 +12,10 @@ export default class Berg extends RoomScene {
 
         /** @type {Phaser.GameObjects.Sprite} */
         this.aqua;
+        /** @type {Phaser.GameObjects.Sprite} */
+        this.winner;
+        /** @type {Phaser.GameObjects.Image[]} */
+        this.sort;
 
 
         /* START-USER-CTR-CODE */
@@ -21,6 +25,7 @@ export default class Berg extends RoomScene {
         this.roomTriggers = {
             'aqua': () => this.triggerGame("AquaGrabber", 916)
         }
+        this.music='249'
 
         /* END-USER-CTR-CODE */
     }
@@ -42,6 +47,43 @@ export default class Berg extends RoomScene {
         const aqua = this.add.sprite(1255, 325, "berg", "aqua0001");
         aqua.setOrigin(0.5, 0.4014336917562724);
 
+        // flags
+        this.add.image(802, 160, "pg-berg", "flags");
+
+        // poles
+        this.add.image(703, 196, "pg-berg", "poles");
+
+        // winner
+        const winner = this.add.sprite(899, 199, "pg-berg", "winner0003");
+
+        // podium
+        this.add.image(502, 254, "pg-berg", "podium");
+
+        // lights_sign
+        this.add.image(139, 238, "pg-berg", "lights_sign");
+
+        // track
+        this.add.image(679, 536, "pg-berg", "track");
+
+        // left_sign
+        const left_sign = this.add.image(280, 327, "pg-berg", "left_sign");
+
+        // right_sign
+        const right_sign = this.add.image(450, 588, "pg-berg", "right_sign");
+
+        // rightest_sign
+        const rightest_sign = this.add.image(1007, 541, "pg-berg", "rightest_sign");
+
+        // top_right_sign
+        this.add.image(1094, 340, "pg-berg", "top_right_sign");
+
+        // mid_sign
+        const mid_sign = this.add.image(480.8026440673506, 441.4079322020519, "pg-berg", "mid_sign");
+        mid_sign.setOrigin(0.5185494832553689, 0.5735995625938831);
+
+        // lists
+        const sort = [left_sign, right_sign, rightest_sign, mid_sign];
+
         // aqua (components)
         const aquaSimpleButton = new SimpleButton(aqua);
         aquaSimpleButton.hoverCallback = () => this.onAquaOver();
@@ -52,6 +94,8 @@ export default class Berg extends RoomScene {
         aquaShowHint.text = "Aqua Grabber";
 
         this.aqua = aqua;
+        this.winner = winner;
+        this.sort = sort;
 
         this.events.emit("scene-awake");
     }
@@ -69,6 +113,7 @@ export default class Berg extends RoomScene {
 
         this.aqua.on('animationcomplete', (animation) => this.onAquaAnimComplete(animation))
         this.aqua.play('aqua_float')
+        this.winner.play('pgwinner')
     }
 
     onAquaAnimComplete(animation) {
