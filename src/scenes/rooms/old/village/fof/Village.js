d8a5f83478c7675d58f421c760875744dd07af6f
dirkdog <leveluforums@gmail.com>
2022-07-11 16:49:03 -0600

    FOF Village

---
+++ src/scenes/rooms/old/village/fof/Village.js
@@ -0,0 +1,182 @@
+import RoomScene from '@scenes/rooms/RoomScene'
+
+import { Animation, Button, MoveTo, SimpleButton } from '@components/components'
+
+
+/* START OF COMPILED CODE */
+
+export default class Village extends RoomScene {
+
+    constructor() {
+        super("Village");
+
+        /** @type {Phaser.GameObjects.Sprite} */
+        this.propvillage;
+        /** @type {Phaser.GameObjects.Sprite} */
+        this.propvillage_1;
+        /** @type {Phaser.GameObjects.Image[]} */
+        this.sort;
+
+
+        /* START-USER-CTR-CODE */
+
+        this.roomTriggers = {
+            'lodge': () => this.triggerRoom(220, 320, 640),
+            'dock': () => this.triggerRoom(800, 436, 280),
+            'beach': () => this.triggerRoom(400, 920, 360),
+            'mtn': () => this.triggerRoom(230, 840, 320),
+            'phone': null
+        }
+		this.music='277'
+
+        /* END-USER-CTR-CODE */
+    }
+
+    /** @returns {void} */
+    _preload() {
+
+        this.load.pack("village-pack", "assets/media/rooms/old/village/village-pack.json");
+    }
+
+    /** @returns {void} */
+    _create() {
+
+        // bg
+        const bg = this.add.image(-25, -15, "village", "bg");
+        bg.setOrigin(0, 0);
+
+        // lights
+        this.add.image(798, 186, "fof-village", "lights");
+
+        // lodge_door
+        const lodge_door = this.add.image(1075, 318, "village", "lodge_door");
+        lodge_door.setOrigin(0.27807486631016043, 0.5560538116591929);
+
+        // lodge_stairs
+        this.add.image(972, 444, "village", "lodge_stairs");
+
+        // lodge_front
+        const lodge_front = this.add.image(1087, 389, "village", "lodge_front");
+        lodge_front.setOrigin(0.4807692307692308, 0.5352112676056338);
+
+        // lodge_snow
+        const lodge_snow = this.add.image(1104, 429, "village", "lodge_snow");
+        lodge_snow.setOrigin(0.5368217054263565, 0.4838709677419355);
+
+        // phone
+        this.add.image(1373, 316, "village", "phone");
+
+        // phone_door
+        const phone_door = this.add.image(1350, 387, "village", "phone_door");
+        phone_door.setOrigin(0.22797927461139897, 0.6206896551724138);
+
+        // phone_snow
+        const phone_snow = this.add.image(1361, 595, "village", "phone_snow");
+        phone_snow.setOrigin(0.509090909090909, 0.5272727272727272);
+
+        // lift
+        this.add.image(348, 200, "village", "lift");
+
+        // smoke
+        const smoke = this.add.sprite(1110, 32, "village", "smoke0001");
+
+        // left_sign
+        const left_sign = this.add.image(133, 677, "village", "left_sign");
+        left_sign.setOrigin(0.4444444444444444, 0.5217391304347826);
+
+        // right_sign
+        const right_sign = this.add.image(1471, 668, "village", "right_sign");
+        right_sign.setOrigin(0.4861111111111111, 0.7831325301204819);
+
+        // tours
+        const tours = this.add.image(792, 483, "village", "tours");
+        tours.setOrigin(0.5, 0.8846153846153846);
+
+        // tours_text
+        const tours_text = this.add.image(794, 505.2201404552512, "village", "tours_text");
+        tours_text.setOrigin(0.5, 3.9212529261510674);
+
+        // chair
+        const chair = this.add.sprite(292, 150, "village", "chair0001");
+
+        // post
+        this.add.image(342, 341, "fof-village", "post");
+
+        // signs
+        this.add.image(835, 153, "fof-village", "signs");
+
+        // balloons
+        this.add.image(917, 382, "fof-village", "balloons");
+
+        // propvillage
+        const propvillage = this.add.sprite(94, 661, "fof-village", "propvillage0001");
+        propvillage.scaleX = 0.35;
+        propvillage.scaleY = 0.35;
+
+        // propholdvillage
+        const propholdvillage = this.add.image(98, 755, "fof-village", "propholdvillage");
+        propholdvillage.scaleX = 0.35;
+        propholdvillage.scaleY = 0.35;
+
+        // propvillage_1
+        const propvillage_1 = this.add.sprite(261, 557, "fof-village", "propvillage0001");
+        propvillage_1.scaleX = 0.3;
+        propvillage_1.scaleY = 0.3;
+
+        // propholdvillage_1
+        const propholdvillage_1 = this.add.image(264, 636, "fof-village", "propholdvillage");
+        propholdvillage_1.scaleX = 0.3;
+        propholdvillage_1.scaleY = 0.3;
+
+        // lists
+        const sort = [tours, tours_text];
+
+        // lodge_door (components)
+        const lodge_doorButton = new Button(lodge_door);
+        lodge_doorButton.spriteName = "lodge_door";
+        lodge_doorButton.activeFrame = false;
+        const lodge_doorMoveTo = new MoveTo(lodge_door);
+        lodge_doorMoveTo.x = 1000;
+        lodge_doorMoveTo.y = 400;
+
+        // phone_door (components)
+        const phone_doorButton = new Button(phone_door);
+        phone_doorButton.spriteName = "phone_door";
+        phone_doorButton.activeFrame = false;
+        const phone_doorMoveTo = new MoveTo(phone_door);
+        phone_doorMoveTo.x = 1340;
+        phone_doorMoveTo.y = 480;
+
+        // smoke (components)
+        const smokeAnimation = new Animation(smoke);
+        smokeAnimation.key = "smoke";
+        smokeAnimation.end = 5;
+
+        // tours (components)
+        const toursSimpleButton = new SimpleButton(tours);
+        toursSimpleButton.callback = () => this.interface.prompt.showItem(428);
+
+        // chair (components)
+        const chairAnimation = new Animation(chair);
+        chairAnimation.key = "chair";
+        chairAnimation.end = 179;
+        chairAnimation.repeatDelay = 1500;
+
+        this.propvillage = propvillage;
+        this.propvillage_1 = propvillage_1;
+        this.sort = sort;
+
+        this.events.emit("scene-awake");
+    }
+
+
+    /* START-USER-CODE */
+    create() {
+        super.create()
+        this.propvillage.play('propvillage')
+		this.propvillage_1.play('propvillage')
+    }
+    /* END-USER-CODE */
+}
+
+/* END OF COMPILED CODE */
